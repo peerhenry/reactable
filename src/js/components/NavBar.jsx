51da0aea1fa65233de91f1bb5b3d4c946c581b10
peerhenry <peerhenry@gmail.com>
2016-08-07 18:28:46 +0200

    add jquery for bootstrap

--- src/js/components/NavBar.jsx
+++ src/js/components/NavBar.jsx
@@ -6,7 +6,19 @@ export default class NavBar extends React.Component{
     <nav className="navbar navbar-inverse navbar-fixed-top">
       <div className="container">
         <ul class="nav navbar-nav">
-          <li><a href="">Options</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              Options <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a href="#">Show header</a>
+              </li>
+              <li>
+                <a href="#">Show title</a>
+              </li>
+            </ul>
+          </li>
           <li><a href="">button2</a></li>
         </ul>
         <form class="navbar-form navbar-right">
