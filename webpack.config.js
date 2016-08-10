51b0d0e61faa8d3e5ae1e86036b6897ca77565a6
PeerHenry <peerhenry@gmail.com>
2016-08-10 22:17:37 +0200

    webpack generation of index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,73 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
+
+function getBabelLoader(){
+  return {
+    test: /\.jsx?$/,
+    exclude: /(node_modules|bower_components|lib)/,
+    loader: 'babel-loader',
+    query: {
+      presets: ['react', 'es2015', 'stage-0'],
+      plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+    }
+  };
+}
+
+function getCssLoader(){
+  return {
+    test: /\.css$/,
+    loader: "style!css"
+  };
+}
+
+function getPugLoader(){
+  return {
+    test: /\.pug$/,
+    loader: "pug-loader"
+  }
+}
+
+function getLoaders()
+{
+  return [
+    getBabelLoader(),
+    getCssLoader(),
+    getPugLoader()
+  ]
+}
+
+function getWebpackPlugin(){
+  return new HtmlWebpackPlugin({
+    template: "index.pug"
+  })
+}
+
+function getPlugins()
+{
+  return debug ? [ getWebpackPlugin() ] : [
+      getWebpackPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+    ]
+}
 
 module.exports = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./jsx/client.jsx",
+  entry: {
+    "client.min.js": "./jsx/client.jsx"
+  },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components|lib)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015', 'stage-0'],
-          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: "style!css"
-      }
-    ]
+    loaders: getLoaders()
   },
   output: {
     path: __dirname + "/build/",
-    filename: "client.min.js"
+    filename: "[name]"
   },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  plugins: getPlugins(),
   devServer: {
     contentBase: "./build/",
     inline: true,
@@ -40,5 +76,5 @@ module.exports = {
       colors: true
     }
   },
-  watch: true
+  //watch: true
 };
